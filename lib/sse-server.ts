d2e7821f654b8db6fbd112ebd53455d7c8ae5077
Charles Lowell <cowboyd@frontside.com>
2026-02-20 13:55:38 -0600

    rename crank -> ui
    It is not an experiment any more

--- lib/sse-server.ts
+++ lib/sse-server.ts
@@ -92,11 +92,11 @@ export function useSSEServer<M extends Methods>(
 
     const ROOT_DIR = join(import.meta.dirname, "..");
     const PUBLIC_DIR = join(
-      ...(ROOT_DIR === "dist" ? [ROOT_DIR, "..", "crank", "dist"] : [ROOT_DIR, "crank", "dist"]),
+      ...(ROOT_DIR === "dist" ? [ROOT_DIR, "..", "ui", "dist"] : [ROOT_DIR, "ui", "dist"]),
     );
 
     const frontendRoutes = ["/live", "/recording", "/demo"];
-    // handle static assets from the crank dist directory (js, css, etc.)
+    // handle static assets from the ui dist directory (js, css, etc.)
     app.use(
       defineEventHandler(async (event) => {
         return await serveStatic(event, {
