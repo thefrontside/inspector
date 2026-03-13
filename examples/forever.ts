c2fcc80a24a0a516f317e8ad0d2e2763cf0382e8
Jacob Bolda <me@jacobbolda.com>
2026-03-12 23:17:13 -0500

    add finally to forever example

--- examples/forever.ts
+++ examples/forever.ts
@@ -1,21 +1,25 @@
 import { main, suspend, spawn, sleep, useAttributes, type Task } from "effection";
 
 await main(function* () {
-  let tasks: Task<string>[] = [];
-  for (let i = 1; i <= 5; i++) {
-    let task = yield* spawn(function* () {
-      yield* useAttributes({ name: "child", number: i });
-      yield* sleep(50 * i);
-      return `child ${i} done`;
-    });
-    tasks.push(task);
-  }
+  try {
+    let tasks: Task<string>[] = [];
+    for (let i = 1; i <= 5; i++) {
+      let task = yield* spawn(function* () {
+        yield* useAttributes({ name: "child", number: i });
+        yield* sleep(50 * i);
+        return `child ${i} done`;
+      });
+      tasks.push(task);
+    }
 
-  for (let t of tasks) {
-    yield* t.halt();
-  }
+    for (let t of tasks) {
+      yield* t.halt();
+    }
 
-  // never finish; keeps the loader running which keeps the UI server up during tests
-  // but also simulates a long-running process that we can attach to
-  yield* suspend();
+    // never finish; keeps the loader running which keeps the UI server up during tests
+    // but also simulates a long-running process that we can attach to
+    yield* suspend();
+  } finally {
+    console.log("shutting down, forever was a fallacy");
+  }
 });
